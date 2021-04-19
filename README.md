64c12d03e7c9c4c6055260226b9e1e030fee12ee
Tmer1168 <Tmer1168@yandex.com>
2021-04-19 16:54:36 +0800

    9

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# yfx-info
+# my_foods_waterfull
 
 ## Project setup
 ```
@@ -13,6 +13,11 @@ npm run serve
 ### Compiles and minifies for production
 ```
 npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
 ```
 
 ### Customize configuration
--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,11 @@
-const Timestamp = new Date().getTime();
+/*
+ * @Author: mingyong.g
+ * @Date: 2020-07-03 22:45:49
+ * @LastEditors: mingyong.g
+ * @LastEditTime: 2020-07-03 22:49:24
+ * @Description: Vue配置文件
+ * @FilePath: \my_foods_waterfull\vue.config.js
+ */
 module.exports = {
-    publicPath: "/multi/",
-
-    // configureWebpack:{
-    //     output: {
-    //         // 输出重构【模块名称.hash值.时间戳】
-    //         filename: `[name].[hash].${Timestamp}.js`,
-    //         chunkFilename: `[name].[hash].${Timestamp}.js`
-    //     },
-    // },
-    // devServer: {
-    //     open: true, //是否自动弹出浏览器页面
-    //     host: "localhost",
-    //     port: '8080',
-    //     https: false,
-    //     hotOnly: false,
-    //     proxy: {
-    //         '/api': {
-    //             target:'https://api.shasta.trongrid.io', //API服务器的地址
-    //             ws: true,  //代理websockets
-    //             changeOrigin: true, // 虚拟的站点需要更管origin
-    //             pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
-    //                 '^/api': '/'
-    //             }
-    //         }
-    //     },
-    // }
-}
+  publicPath: "/multi/"
+};
