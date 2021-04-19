bf1b10960074e98216ad42ea104f1a749ae67bb4
Tmer1168 <Tmer1168@yandex.com>
2021-04-19 16:12:24 +0800

    7

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,29 @@
 const Timestamp = new Date().getTime();
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/YOUR_REPO_NAME/' : '/',
+    publicPath: "/multi/",
 
-    configureWebpack:{
-        output: {
-            // 输出重构【模块名称.hash值.时间戳】
-            filename: `[name].[hash].${Timestamp}.js`,
-            chunkFilename: `[name].[hash].${Timestamp}.js`
-        },
-    },
-    devServer: {
-        open: true, //是否自动弹出浏览器页面
-        host: "localhost",
-        port: '8080',
-        https: false,
-        hotOnly: false,
-        proxy: {
-            '/api': {
-                target:'https://api.shasta.trongrid.io', //API服务器的地址
-                ws: true,  //代理websockets
-                changeOrigin: true, // 虚拟的站点需要更管origin
-                pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
-                    '^/api': '/'
-                }
-            }
-        },
-    }
+    // configureWebpack:{
+    //     output: {
+    //         // 输出重构【模块名称.hash值.时间戳】
+    //         filename: `[name].[hash].${Timestamp}.js`,
+    //         chunkFilename: `[name].[hash].${Timestamp}.js`
+    //     },
+    // },
+    // devServer: {
+    //     open: true, //是否自动弹出浏览器页面
+    //     host: "localhost",
+    //     port: '8080',
+    //     https: false,
+    //     hotOnly: false,
+    //     proxy: {
+    //         '/api': {
+    //             target:'https://api.shasta.trongrid.io', //API服务器的地址
+    //             ws: true,  //代理websockets
+    //             changeOrigin: true, // 虚拟的站点需要更管origin
+    //             pathRewrite: {   //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
+    //                 '^/api': '/'
+    //             }
+    //         }
+    //     },
+    // }
 }
